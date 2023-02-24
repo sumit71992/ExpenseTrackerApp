2ee22a917e6c9c4cbdaa4b282cb35a7831f86524
sumit71992 <sumit71992@gmail.com>
2023-02-24 22:20:41 +0530

    signin frontend done

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,11 +15,29 @@ const signup = (req, res, next) => {
           })
           .catch((err) => console.log(err));
       } else {
-        return res.json({message:"User already signed up"})
+        return res.json({ message: "User already signed up" });
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+const signin = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ where: { email: email } })
+    .then((usr) => {
+      if (!usr) {
+        return res.json({ message: "Email not registered" });
+      } else if (usr.password === password) {
+        console.log("success");
+        return res.json({ message: "login success" });
+      } else {
+        console.log("Invalid");
+        return res.json({ message: "Password Invalid" });
       }
     })
     .catch((err) => console.log(err));
 };
 module.exports = {
   signup,
+  signin,
 };
