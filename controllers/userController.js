87aa407c3b6e00aa034021141c56d631d5b2b55b
sumit71992 <sumit71992@gmail.com>
2023-02-24 23:48:24 +0530

    stored hashed password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,23 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const signup = (req, res, next) => {
   const { name, email, password } = req.body;
   User.findOne({ where: { email: email } })
     .then((signedUser) => {
       if (!signedUser) {
-        User.create({
-          name,
-          email,
-          password,
-        })
-          .then((user) => {
-            return res.json("sign up successfully");
+        bcrypt
+          .hash(password, 10)
+          .then((hashedPwd) => {
+            User.create({
+              name,
+              email,
+              password: hashedPwd,
+            })
+              .then((user) => {
+                return res.json("sign up successfully");
+              })
+              .catch((err) => console.log(err));
           })
           .catch((err) => console.log(err));
       } else {
@@ -21,21 +27,34 @@ const signup = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-const signin = (req, res, next) => {
-  const { email, password } = req.body;
-  User.findOne({ where: { email: email } })
-    .then((usr) => {
-      if (!usr) {
-        return res.status(404).json({ message: "Email not registered" });
-      } else if (usr.password === password) {
-        console.log("success");
-        return res.json({ message: "login success" });
-      } else {
-        console.log("Invalid");
-        return res.status(401).json({ message: "Password Invalid" });
-      }
-    })
-    .catch((err) => console.log(err));
+const signin = async(req, res, next) => {
+    try{
+        const { email, password } = req.body;
+        await User.findOne({ where: { email: email }})
+        .then((usr) => {
+            if (!usr) {
+              return res.status(404).json({ message: "Email not registered" });
+            } else {
+               bcrypt
+                .compare(password, usr.password)
+                .then((result) => {
+                  if (!result) {
+                    return res.status(401).json({ message: "Password Invalid" });
+                  } else {
+                    return res.json({ message: "login success" });
+                  }
+                })
+                .catch((err) => console.log(err));
+            }
+          })
+          .catch((err) => console.log(err));
+    }
+    catch(err){
+        return res.status(500).json({message:err});
+    }
+  
+  
+    
 };
 module.exports = {
   signup,
