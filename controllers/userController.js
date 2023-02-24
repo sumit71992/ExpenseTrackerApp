f323071561d7878eb9b0bd9c253007414dabdd75
sumit71992 <sumit71992@gmail.com>
2023-02-24 19:51:21 +0530

    signup validation to check user already registerd or not done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,25 @@
-const User = require('../models/userModel');
+const User = require("../models/userModel");
 
-const signup = (req, res, next) =>{
-    const {name,email,password} = req.body;
-    console.log(name,email,password)
-    User.create({
-        name,
-        email,
-        password
-    }).then(user=>{
-        return res.json("sign up successfully");
-    }).catch(err=>console.log(err))
-    
-}
-module.exports={
-signup,
-}
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+  User.findOne({ where: { email: email } })
+    .then((signedUser) => {
+      if (!signedUser) {
+        User.create({
+          name,
+          email,
+          password,
+        })
+          .then((user) => {
+            return res.json("sign up successfully");
+          })
+          .catch((err) => console.log(err));
+      } else {
+        return res.json({message:"User already signed up"})
+      }
+    })
+    .catch((err) => console.log(err));
+};
+module.exports = {
+  signup,
+};
