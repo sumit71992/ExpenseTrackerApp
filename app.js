e215ef0d3cf3c98a0432397ce5f19bd830192fa4
sumit71992 <sumit71992@gmail.com>
2023-03-05 00:47:13 +0530

    credential moved to env

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ require("dotenv").config();
 
 const app = express();
 app.use(compression());
-const privateKey = fs.readFileSync("server.key");
-const certificate = fs.readFileSync("server.cert");
+// const privateKey = fs.readFileSync("server.key");
+// const certificate = fs.readFileSync("server.cert");
 
 const mainRoutes = require("./routes/expenseRoute");
 const userRoutes = require("./routes/userRoute");
--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expensetracker','root','kbicky',{
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
     dialect: 'mysql',
     host: 'localhost'
 });
